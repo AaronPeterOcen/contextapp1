057ee7cd91a504001ac9e8d73d113bfc5a9d4d48
OAaronPeter <ocenaaronpeter2001@gmail.com>
2022-07-20 18:31:47 +0300

    finished context comps

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Booklist from './components/Booklist';
 import Navbar from './components/Navbar';
 import Themetoggle from './components/Themetoggle';
+import AuthContextProvider from './context/Authcontext';
 import ThemeContextProvider from './context/ThemeContext';
 
  function App() {
   return (
     <div className='App'>
       <ThemeContextProvider>
-      <Navbar />
-      <Booklist />
-      <Themetoggle />
+        <AuthContextProvider>
+          <Navbar />
+          <Booklist />
+          <Themetoggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+import { AuthContext } from '../context/Authcontext';
 import { ThemeContext } from '../context/ThemeContext';
 
 class Navbar extends Component {
-    static contextType = ThemeContext
     render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <nav style={{ backkground: theme.ui, color: theme.syntax}}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Hime</li>
-                    <li>About</li>
-                    <li>Others</li>
-                </ul>
-            </nav>
+        return(
+        <AuthContext.Consumer>{( authContext) =>(
+            <ThemeContext.Consumer>{(themeContext) => {
+                const { isAuthenticated, toggleAuth } = authContext;
+                const { isLightTheme, light, dark } = themeContext;
+                const theme = isLightTheme ? light : dark;
+                return (
+                    <nav style={{ background: theme.ui, color: theme.syntax}}>
+                        <h1>Context App</h1>
+                        <div onClick={toggleAuth}>
+                            { isAuthenticated ? 'Logged in' : 'Logged out'}
+                        </div>
+                        <ul>
+                            <li>Hime</li>
+                            <li>About</li>
+                            <li>Others</li>
+                        </ul>
+                    </nav>
+                )
+            }}</ThemeContext.Consumer>
+        )}</AuthContext.Consumer>
+       
+        
         );
     }
 }
